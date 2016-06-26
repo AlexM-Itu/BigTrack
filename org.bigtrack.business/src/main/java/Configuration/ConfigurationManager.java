eb5801f61c91844ab1f65346a594cc5e85982fcf
Alex <AlexTheProgrammerMail@gmail.com>
2016-06-25 22:29:27 -0700

    Configuration loading and reflection implemented

--- org.bigtrack.business/src/main/java/Configuration/ConfigurationManager.java
+++ org.bigtrack.business/src/main/java/Configuration/ConfigurationManager.java
@@ -1,8 +1,18 @@
 package Configuration;
 
 import Database.DatabaseManager;
+import com.thoughtworks.xstream.XStream;
+import javafx.scene.text.TextBuilder;
 import lombok.Getter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.lang.String;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.UUID;
 import java.util.List;
 
@@ -12,13 +22,18 @@ import java.util.List;
 public class ConfigurationManager {
     private static ConfigurationManager ourInstance = new ConfigurationManager();
     private BigTrackConfiguration bigTrackConfiguration;
+    private final String configurationFileName = "bigTrack.xml";
 
     public static ConfigurationManager getInstance() {
         return ourInstance;
     }
 
     private ConfigurationManager() {
-        // todo load conf. from XML and initialize dialect drivers
+        XStream serializer = new XStream();
+        bigTrackConfiguration = (BigTrackConfiguration) serializer.fromXML (new File(configurationFileName));
+        for (DatabaseConfiguration configuration : bigTrackConfiguration.getDatabaseConfigurations()){
+            configuration.setDialectDriver(Class.forName(configuration.getDialectDriverName().newInstance()));
+        }
     }
 
     public List<DatabaseConfiguration> getDatabaseConfigurations(){
